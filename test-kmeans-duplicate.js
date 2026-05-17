497fa674318db05560f0f41b73a87d16ba4288db
abdullah-binmadhi <89339365+abdullah-binmadhi@users.noreply.github.com>
2026-05-18 04:11:09 +0800

    refactor: remove obsolete ML test scripts and documentation, and add project skill index tracker

--- test-kmeans-duplicate.js
+++
@@ -1,9 +0,0 @@
-import { kmeans } from 'ml-kmeans';
-
-try {
-    const data = [[0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0]]; 
-    kmeans(data, 4, { initialization: 'kmeans++' }); 
-    console.log("Success");
-} catch (e) {
-    console.log("KMeans Crash:", e.message);
-}
--- test-kmeans-k.js
+++
@@ -1,9 +0,0 @@
-import { kmeans } from 'ml-kmeans';
-
-try {
-    const data = [[1, 2], [2, 3], [3, 4]]; // N = 3
-    kmeans(data, 4, { initialization: 'kmeans++' }); // K = 4
-    console.log("Success");
-} catch (e) {
-    console.log("KMeans Crash:", e.message);
-}
